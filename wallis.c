1a51e9077171e56968927939b9a8ee379b92f949
shifashareef <85341917+shifashareef@users.noreply.github.com>
2021-06-04 15:54:05 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -1,7 +1,7 @@
-#include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
+#include<assert.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
 
 float wallis_pi(int);
 
